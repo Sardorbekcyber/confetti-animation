9cf04bed49d14adf6693b636c2c00e81001a5df7
Sardor Narziyev <sardornarziyev020800@gmail.com>
2021-04-17 02:49:46 +0500

    Implemented Simple Confetti Animation

--- app/src/main/java/com/turbosoft/confettianimation/MainActivity.kt
+++ app/src/main/java/com/turbosoft/confettianimation/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.turbosoft.confettianimation
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.turbosoft.confettianimation.databinding.ActivityMainBinding
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding) {
+            viewKonfetti.build()
+                    .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                    .setDirection(0.0, 359.0)
+                    .setSpeed(1f, 5f)
+                    .setFadeOutEnabled(true)
+                    .setTimeToLive(2000L)
+                    .addShapes(Shape.Square, Shape.Circle)
+                    .addSizes(Size(12))
+                    .setPosition(-50f, viewKonfetti.width + 50f, -50f, -50f)
+                    .streamFor(300, 5000L)
+        }
     }
 }
